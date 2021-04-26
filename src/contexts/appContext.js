7c3518d8ad4f1371c2b1364e377ad8ec30c2dcea
WahidX <wahidx93@gmail.com>
2021-04-26 17:07:23 +0530

    initial color fix

--- src/contexts/appContext.js
+++ src/contexts/appContext.js
@@ -1,11 +1,10 @@
 import React, { createContext, useState } from "react";
-import { getRandomColor } from "../utils";
 
 export const AppDataContext = createContext();
 
 export const AppDataProvider = (props) => {
 	const [appData, setAppData] = useState({
-		color: getRandomColor(),
+		color: "black",
 		changeColor: false,
 	});
 
